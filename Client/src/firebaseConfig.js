efab60bf60dcfd7c99dbacb8977bdf5b877705bb
Deepak yadav <deepaksyadav04@gmail.com>
2024-09-24 10:51:04 +0530

    changes the firebaseConfig

--- Client/src/firebaseConfig.js
+++ Client/src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsql3YFoweTiaRZsQzx_-_RDiPeeeGjEA",
-  authDomain: "codeassist-aaa95.firebaseapp.com",
-  projectId: "codeassist-aaa95",
-  storageBucket: "codeassist-aaa95.appspot.com",
-  messagingSenderId: "670961398862",
-  appId: "1:670961398862:web:9eb5c979414e97a5c47f1f",
-  measurementId: "G-91RDZ25CS8"
+  apiKey: "AIzaSyBib-vLsY46atydKoOZt0gSLA__AMi1l8I",
+  authDomain: "codeassist-ecf6d.firebaseapp.com",
+  projectId: "codeassist-ecf6d",
+  storageBucket: "codeassist-ecf6d.appspot.com",
+  messagingSenderId: "631057021237",
+  appId: "1:631057021237:web:d1855885d3395cdada347c",
+  measurementId: "G-JD49RPRB29"
 };
 
 const app = initializeApp(firebaseConfig);
